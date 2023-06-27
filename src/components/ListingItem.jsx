f0ad5f04ca4b910363bfcbfcdbf2c5b0bbc376c6
Nwosa <obere4u@gmail.com>
2023-06-27 17:18:26 +0100

    Listing item component completed

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,20 +5,29 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing, id }) {
   return (
-    <li className="bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150">
-      <Link to={`/category/${listing.type}/${id}`}>
+    <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-3">
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
         <img
           src={listing.imgUrls[0]}
           alt="Listing Image"
-        className="h-[170px] w-full object-cover hover:scale-150 transition-scale duration-200 ease-in"/>
-        <Moment fromNow>{listing.timeStamp?.toDate()}</Moment>
-        <div>
-          <div>
-            <MdLocationOn />
-            <p>{listing.address}</p>
+          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
+          loading="lazy"
+        />
+        <Moment
+          fromNow
+          className="absolute top-2 right-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
+        >
+          {listing.timeStamp?.toDate()}
+        </Moment>
+        <div className="w-full p-2">
+          <div className="flex items-center space-x-1">
+            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <p className="font-semibold text-sm mb-[2px] text-grey-600 truncate">
+              {listing.address}
+            </p>
           </div>
-          <p>{listing.name}</p>
-          <p>
+          <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
+          <p className="text-[#457b93] mt-2 font-semibold">
             $
             {listing.offer
               ? listing.discountPrice
@@ -29,15 +38,15 @@ export default function ListingItem({ listing, id }) {
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             {listing.type === "rent" && " / month"}
           </p>
-          <div>
-            <div>
-              <p>
+          <div className="flex items-center mt-[10px] space-x-3">
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">
                 {" "}
                 {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
               </p>
             </div>
-            <div>
-              <p>
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">
                 {listing.bathrooms > 1
                   ? `${listing.bathrooms} Baths`
                   : "1 Bath"}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -153,7 +153,7 @@ function onChange(e) {
         {!loading && listings.length > 0 && (
           <>
             <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 my-6">
               {listings.map((listing) => {
                 return (
                   <ListingItem
