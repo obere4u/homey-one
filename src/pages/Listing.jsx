c2de741b0047b0e43b8b64c06c131a36c362e145
Nwosa <obere4u@gmail.com>
2023-11-29 13:50:34 +0100

    Implement Skeleton Loading on Listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -23,35 +23,44 @@ import {
 import { getAuth } from "firebase/auth";
 import Contact from "./Contact";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+// ... (import statements)
 
 export default function Listing() {
   const params = useParams();
   const auth = getAuth();
   const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
   const [contactOwner, setContactOwner] = useState(false);
+  const [type, setType] = useState("");
 
   useEffect(() => {
     async function fetchListing() {
       const docRef = doc(db, "listings", params.listingId);
       const docSnap = await getDoc(docRef);
+
       if (docSnap.exists()) {
         setListing(docSnap.data());
-        setLoading(false);
+        setType(docSnap.data().type);
       }
     }
-
     fetchListing();
   }, [params.listingId]);
-
-  if (loading) {
-    return <Spinner />;
-  }
 
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
   };
+
+  if (!listing) {
+    return (
+      <div className="max-w-6xl mx-auto space-y-4">
+        <SwiperSkeleton />
+        <ListingDetailsSkeleton />
+      </div>
+    );
+  }
 
   const formattedDiscountPrice = listing.discountPrice
     ?.toString()
@@ -67,39 +76,43 @@ export default function Listing() {
 
   return (
     <main>
-      <Swiper
-        modules={[A11y, Navigation, Pagination, Autoplay, EffectFade]}
-        slidesPerView={1}
-        navigation
-        pagination={{ type: "progressbar" }}
-        effect="fade"
-        autoplay={{ delay: 3000 }}
-      >
-        {listing &&
-          listing.imgUrls.map((url, index) => (
-            <SwiperSlide key={index}>
-              <div
-                className="relative  h-[300px] overflow-hidden"
-                style={{
-                  background: `url(${url}) center no-repeat`,
-                  backgroundSize: "cover",
-                }}
-              ></div>
-            </SwiperSlide>
-          ))}
-        <div
-          className="absolute top-[77%]  right-4 z-10 cursor-pointer border-2 border-gray-400 rounded-full w-10 h-10 flex justify-center items-center bg-white"
-          onClick={() => {
-            navigator.clipboard.writeText(window.location.href);
-            setShareLinkCopied(true);
-            setTimeout(() => {
-              setShareLinkCopied(false);
-            }, 2000);
-          }}
+      {listing ? (
+        <Swiper
+          modules={[A11y, Navigation, Pagination, Autoplay, EffectFade]}
+          slidesPerView={1}
+          navigation
+          pagination={{ type: "progressbar" }}
+          effect="fade"
+          autoplay={{ delay: 3000 }}
         >
-          <FaShare className="text-lg text-slate-600" />
-        </div>
-      </Swiper>
+          {listing &&
+            listing.imgUrls.map((imgUrl, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="relative  h-[300px] overflow-hidden"
+                  style={{
+                    background: `url(${imgUrl}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          <div
+            className="absolute top-[77%]  right-4 z-10 cursor-pointer border-2 border-gray-400 rounded-full w-10 h-10 flex justify-center items-center bg-white"
+            onClick={() => {
+              navigator.clipboard.writeText(window.location.href);
+              setShareLinkCopied(true);
+              setTimeout(() => {
+                setShareLinkCopied(false);
+              }, 2000);
+            }}
+          >
+            <FaShare className="text-lg text-slate-600" />
+          </div>
+        </Swiper>
+      ) : (
+        <SwiperSkeleton />
+      )}
 
       {shareLinkCopied && (
         <span className="fixed top-[22%] right-[1%] text-white font-semibold z-10  bg-slate-500 rounded p-2">
@@ -108,69 +121,27 @@ export default function Listing() {
       )}
 
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg lg:space-x-5 md:space-x-5">
-        <div className="w-full ">
-          <p className="text-xl md:text-2xl font-bold mb-3 text-blue-900">
-            {/* adds coma after every 3 digits (.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")) */}
-            {listing.name} - $
-            {listing.offer ? formattedDiscountPrice : formattedRegularPrice}
-            {listing.type === "rent" ? " / Month" : ""}
-          </p>
-          <p className="flex items-center my-3 font-semibold ">
-            <FaMapMarkerAlt className="text-green-700 mr-1" />
-            {listing.address}
-          </p>
-          <div className="flex justify-start items-center space-x-4 ">
-            <p className=" bg-red-800 w-full max-w-[200px] rounded-md p-1.5 text-white text-center font-semibold shadow-md">
-              {listing.type === "rent" ? "Rent" : "Sale"}
-            </p>
-            {listing.type === "sale" &&
-              <p className=" bg-green-800 w-full max-w-[300px] rounded-md p-1.5 text-white text-center font-semibold shadow-md">
-                <p>
-                  ${discount} discount
-                </p>
-              </p>
-            }
-          </div>
-          <p className="my-3">
-            <span className="font-semibold">Description - </span>
-            {listing.description}
-          </p>
-          <ul className="flex justify-between items-center text-sm font-semibold flex-wrap">
-            <li className="flex items-center whitespace-nowrap">
-              <FaBed className="text-lg mr-1" />
-              {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
-            </li>
-            <li className="flex items-center whitespace-nowrap">
-              <FaBath className="text-lg mr-1" />
-              {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
-            </li>
-            <li className="flex items-center whitespace-nowrap">
-              <FaParking className="text-lg mr-1" />
-              {listing.park ? "Parking Spot" : "No Parking"}
-            </li>
-            <li className="flex items-center mt-3 md:mt-0  whitespace-nowrap">
-              <FaChair className="text-lg mr-1" />
-              {listing.furnish ? "Furnished" : "Not Furnished"}
-            </li>
-          </ul>
-          {listing.userRef !== auth.currentUser?.uid && !contactOwner && (
-            <div className="mt-6">
-              <button
-                onClick={() => setContactOwner(true)}
-                className="px-7 py-3 bg-blue-600 text-white font-medium  text-sm uppercase rounded shadow-medium hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-100 ease-in-out"
-              >
-                Contact Owner
-              </button>
-            </div>
-          )}
-          {contactOwner && (
-            <Contact
-              userRef={listing.userRef}
-              listing={listing}
-            />
-          )}
-        </div>
-        <div className="h-[200px] md:h-[400px] w-full z-10 overflow-x-hidden mt-5 md:mt-0">
+        {listing && (
+          <ListingDetails
+            name={listing.name}
+            offer={listing.offer}
+            formattedDiscountPrice={formattedDiscountPrice}
+            formattedRegularPrice={formattedRegularPrice}
+            type={type}
+            address={listing.address}
+            discount={discount}
+            description={listing.description}
+            bedrooms={listing.bedrooms}
+            bathrooms={listing.bathrooms}
+            park={listing.park}
+            furnish={listing.furnish}
+            auth={auth}
+            userRef={listing.userRef}
+            contactOwner={contactOwner}
+            setContactOwner={setContactOwner}
+          />
+        )}
+        <div className="h-[200px] md:h-[400px] w-full z-10 overflow-x-hidden mt-5 md:mt-0 border">
           <MapContainer
             center={[listing.geolocation.lat, listing.geolocation.long]}
             zoom={13}
@@ -192,3 +163,166 @@ export default function Listing() {
     </main>
   );
 }
+
+// Skeleton components
+const SwiperSkeleton = () => (
+  <div className="w-full max-w-6xl">
+    <Skeleton height={300} />
+  </div>
+);
+
+const ListingDetailsSkeleton = (type) => {
+  return (
+    <div className="md:flex space-y-5 space-x-3">
+      <div className="w-full pl-4">
+        {/* Listing Name skeleton */}
+        <div className="mb-2 pr-5">
+          <Skeleton
+            height={50}
+            width={400}
+          />
+        </div>
+        {/* Listing address skeleton */}
+        <div className="mb-2">
+          <Skeleton
+            height={30}
+            width={150}
+          />
+        </div>
+        {/* Listing Type skeleton */}
+        <div className="mb-2 flex items-center gap-20">
+          {/* Conditionally render discounted price based on the type */}
+          {type === "sale" && (
+            <div className="">
+              <Skeleton
+                height={50}
+                width={250}
+              />
+            </div>
+          )}
+        </div>
+        {/* Listing Description skeleton */}
+        <div>
+          <Skeleton
+            height={20}
+            width={150}
+          />
+        </div>
+        {/* Listing features skeleton */}
+        <div className="flex justify-between items-enter mt-4">
+          <div>
+            <Skeleton
+              height={20}
+              width={100}
+            />
+          </div>
+          <div>
+            <Skeleton
+              height={20}
+              width={100}
+            />
+          </div>
+          <div>
+            <Skeleton
+              height={20}
+              width={100}
+            />
+          </div>
+          <div>
+            <Skeleton
+              height={20}
+              width={100}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="w-full pr-4">
+        <div className="w-full h-[100%]">
+          <Skeleton height={450} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ListingDetails = ({
+  name,
+  offer,
+  formattedDiscountPrice,
+  formattedRegularPrice,
+  type,
+  address,
+  discount,
+  description,
+  bedrooms,
+  bathrooms,
+  park,
+  furnish,
+  auth,
+  userRef,
+  contactOwner,
+  setContactOwner,
+}) => {
+  return (
+    <div className="w-full ">
+      <p className="text-xl md:text-2xl font-bold mb-3 text-blue-900">
+        {name} - ${offer ? formattedDiscountPrice : formattedRegularPrice}
+        {type === "rent" ? " / Month" : ""}
+      </p>
+      <p className="flex items-center my-3 font-semibold ">
+        <FaMapMarkerAlt className="text-green-700 mr-1" />
+        {address}
+      </p>
+      <div className="flex justify-start items-center space-x-4 ">
+        <p className=" bg-red-800 w-full max-w-[200px] rounded-md p-1.5 text-white text-center font-semibold shadow-md">
+          {type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {type === "sale" ? (
+          <div className=" bg-green-800 w-full max-w-[300px] rounded-md p-1.5 text-white text-center font-semibold shadow-md">
+            <p>${discount} discount</p>
+          </div>
+        ) : (
+          <div className="border border-green-700">
+            <Skeleton
+              height={50}
+              width={100}
+            />
+          </div>
+        )}
+      </div>
+      <p className="my-3">
+        <span className="font-semibold">Description - </span>
+        {description}
+      </p>
+      <ul className="grid grid-cols-3 lg:grid-cols-4 xlg:grid-cols-4 2xl:grid-cols-4 gap-2">
+        <li className="flex items-center whitespace-nowrap">
+          <FaBed className="text-lg mr-1" />
+          {bedrooms > 1 ? `${bedrooms} Beds` : "1 Bed"}
+        </li>
+        <li className="flex items-center whitespace-nowrap">
+          <FaBath className="text-lg mr-1" />
+          {bathrooms > 1 ? `${bathrooms} Baths` : "1 Bath"}
+        </li>
+        <li className="flex items-center whitespace-nowrap">
+          <FaParking className="text-lg mr-1" />
+          {park ? "Parking Spot" : "No Parking"}
+        </li>
+        <li className="flex items-center whitespace-nowrap">
+          <FaChair className="text-lg mr-1 " />
+          {furnish ? "Furnished" : "Not Furnished"}
+        </li>
+      </ul>
+      {userRef !== auth.currentUser?.uid && !contactOwner && (
+        <div className="mt-6">
+          <button
+            onClick={() => setContactOwner(true)}
+            className="px-7 py-3 bg-blue-600 text-white font-medium  text-sm uppercase rounded shadow-medium hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-100 ease-in-out"
+          >
+            Contact Owner
+          </button>
+        </div>
+      )}
+      {contactOwner && <Contact userRef={userRef} />}
+    </div>
+  );
+};
